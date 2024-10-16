ab88e78f62dec7538714ba9a3bb319e11ab1900b
steve <sdmatthews01@gmail.com>
2024-10-15 21:37:51 -0400

    added polling

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { VoteService } from '../vote.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
@@ -13,65 +14,74 @@ import { catchError } from 'rxjs/operators';
 })
 export class MainPageComponent implements OnInit{
 
-private voteService = inject(VoteService);
+  private voteService = inject(VoteService);
 
-either: string = 'TRUMP';
-or: string = 'HARRIS';
-canVoteToday: boolean = true;
-eitherVoteCount = 9;
-orVoteCount = 9;
+  either: string = 'TRUMP';
+  or: string = 'HARRIS';
+  canVoteToday: boolean = true;
+  eitherVoteCount: number = 0;
+  orVoteCount: number = 0;
+  pollingInterval = 5000; //ms
 
-constructor() {}
+  constructor() {}
 
-ngOnInit(): void {
-  //check to see if this person has voted today
-  this.canVoteToday = this.canVote();
-  //get vote counts
-  this.getVoteCounts();
-}
-
-countVote($event: 'either' | 'or'): void {
-  //check the sender and increment the appropriate count
-  if (!this.canVoteToday){
-    alert('Sorry, you already voted today');
-    return;
+  ngOnInit(): void {
+    //check to see if this person has voted today
+    this.canVoteToday = this.canVote();
+    //get vote counts
+    this.getVoteCounts();
   }
 
-  this.voteService.addVote($event).subscribe((res: VoteCounts) => {
-      if (res) {
-        this.setVotedStatus();
-        this.updateVoteCounts(res);
-      } else {
-        alert('There was an error processing your vote. Please try again');
-      }
-  });
-  
-}
+  countVote($event: 'either' | 'or'): void {
+    //check the sender and increment the appropriate count
+    if (!this.canVoteToday){
+      alert('Sorry, you already voted today');
+      return;
+    }
 
-updateVoteCounts(counts: VoteCounts): void {
-  this.eitherVoteCount = counts.either;
-  this.orVoteCount = counts.or;
-}
+    this.voteService.addVote($event).subscribe((res: VoteCounts) => {
+        if (res) {
+          this.setVotedStatus();
+          this.updateVoteCounts(res);
+        } else {
+          alert('There was an error processing your vote. Please try again');
+        }
+    });
+    
+  }
 
-setVotedStatus(): void {
-  const today = new Date().toDateString();
-  localStorage.setItem('lastEitherOrVoteDate', today);
-  this.canVoteToday = false;
-}
+  updateVoteCounts(counts: VoteCounts): void {
+    this.eitherVoteCount = counts.either;
+    this.orVoteCount = counts.or;
+  }
 
-canVote(): boolean {
-  const lastVoteDate = localStorage.getItem('lastEitherOrVoteDate');
-  const today  = new Date().toDateString();
-  const didVoteToday = lastVoteDate != today;
-  return didVoteToday;
-}
+  setVotedStatus(): void {
+    const today = new Date().toDateString();
+    localStorage.setItem('lastEitherOrVoteDate', today);
+    this.canVoteToday = false;
+  }
 
-getVoteCounts(): void {
-  this.voteService.getVoteCounts().subscribe(votes => {
-    this.eitherVoteCount = votes.either;
-    this.orVoteCount = votes.or;
-  });
-}
+  canVote(): boolean {
+    const lastVoteDate = localStorage.getItem('lastEitherOrVoteDate');
+    const today  = new Date().toDateString();
+    const didVoteToday = lastVoteDate != today;
+    return didVoteToday;
+  }
+
+  getVoteCounts(): void {
+    this.startVoteCountPolling();
+  }
+
+  startVoteCountPolling(): void {
+    interval(this.pollingInterval)
+      .pipe(
+        switchMap(() => this.voteService.getVoteCounts())
+      )
+      .subscribe(votes => {
+        this.eitherVoteCount = votes.either;
+        this.orVoteCount = votes.or;
+      });    
+  }
 
 }
 
